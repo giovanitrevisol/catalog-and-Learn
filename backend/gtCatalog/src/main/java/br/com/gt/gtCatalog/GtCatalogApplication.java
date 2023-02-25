3a98128542e1151aa0c5eff67c5443385237a394
giovani <giovani.trevisol@cwi.com.br>
2023-02-24 22:41:16 -0300

    created backend project

---
+++ backend/gtCatalog/src/main/java/br/com/gt/gtCatalog/GtCatalogApplication.java
@@ -0,0 +1,13 @@
+package br.com.gt.gtCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtCatalogApplication.class, args);
+	}
+
+}
---
+++ backend/gtCatalog/src/test/java/br/com/gt/gtCatalog/GtCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gt.gtCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
